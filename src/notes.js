3a08164b76d4943b461d0f2cc46e7653733e8f54
Michael Lappert <mi.lappert@gmail.com>
2024-01-24 08:10:26 +0100

    update

--- src/notes.js
+++ src/notes.js
@@ -22,6 +22,12 @@ const notes = [
     content:
       "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software.",
   },
+  {
+    key: 5,
+    title: 'Interplay of Sensitivity and Experience',
+    content:
+      'One cannot have experiences without the necessary sensitivity, and one cannot develop sensitivity without having a wide range of experiences.',
+  },
 ];
 
 export default notes;
