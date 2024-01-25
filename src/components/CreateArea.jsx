ef9c3d4764fc2bae7ae9eb248e9a67746a00710c
Michael Lappert <mi.lappert@gmail.com>
2024-01-25 19:41:48 +0100

    update

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function CreateArea() {
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: '',
+    content: '',
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
   return (
     <div>
       <form>
-        <input name='title' placeholder='Title' />
-        <textarea name='content' placeholder='Take a note...' rows='3' />
-        <button>Add</button>
+        <input name='title' placeholder='Title' onChange={handleChange} value={note.title} />
+        <textarea
+          name='content'
+          placeholder='Take a note...'
+          rows='3'
+          onChange={handleChange}
+          value={note.content}
+        />
+        <button
+          onClick={event => {
+            props.onAdd(note);
+            setNote({
+              title: '',
+              content: '',
+            });
+            event.preventDefault();
+          }}>
+          Add
+        </button>
       </form>
     </div>
   );
