4ccc66211637c05c27e7348bdf906e37d7ae77cf
Michael Lappert <mi.lappert@gmail.com>
2024-01-25 18:57:40 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-//1. Create a new React app.
-//2. Create a App.jsx component.
-//3. Create a Header.jsx component that renders a <header> element
-//to show the Keeper App name in an <h1>.
-//4. Create a Footer.jsx component that renders a <footer> element
-//to show a copyright message in a <p> with a dynamically updated year.
-//5. Create a Note.jsx component to show a <div> element with a
-//<h1> for a title and a <p> for the content.
-//6. Make sure that the final website is styled like the example shown here:
-//https://l1pp6.csb.app/
-
-//HINT: You will need to study the classes in teh styles.css file to appy styling.
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// Steps:
+// 1. Implement the add note functionality
+// 1.1. Create a constant that keeps track of the title and content
+// 1.1.1. Use useState to store the title and content in the state
+// 1.2. Pass the new note back to the App.js
+// 1.3. Add new note to an array
+
+// 2. Implement the delete note functionality
+// 2.1. Add a delete button next to each note
+// 2.2. Implement the function to delete a note
+// 2.2.1. Pass a id over to the App
+// 2.2.2. Add the logic to remove a note from the array
