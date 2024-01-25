fa088291816206e0267bd4691a38a251082b1f95
Michael Lappert <mi.lappert@gmail.com>
2024-01-25 19:11:14 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import Header from './Header';
-import Note from './Note';
 import Footer from './Footer';
-import notes from '../notes.js';
+import Note from './Note';
+import CreateArea from './CreateArea';
 
 function App() {
   return (
     <div>
       <Header />
-      {notes.map(note => {
-        return <Note key={note.key} title={note.title} content={note.content} />;
-      })}
+      <CreateArea />
+      <Note key={1} title='Note title' content='Note content' />
       <Footer />
     </div>
   );
