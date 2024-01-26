cdb50b806c96451d1938a94006f1937af15bf417
Michael Lappert <mi.lappert@gmail.com>
2024-01-26 07:46:35 +0100

    update

--- src/index.js
+++ src/index.js
@@ -3,16 +3,3 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// Steps:
-// 1. Implement the add note functionality
-// 1.1. Create a constant that keeps track of the title and content
-// 1.1.1. Use useState to store the title and content in the state
-// 1.2. Pass the new note back to the App.js
-// 1.3. Add new note to an array
-
-// 2. Implement the delete note functionality
-// 2.1. Add a delete button next to each note
-// 2.2. Implement the function to delete a note
-// 2.2.1. Pass a id over to the App
-// 2.2.2. Add the logic to remove a note from the array
